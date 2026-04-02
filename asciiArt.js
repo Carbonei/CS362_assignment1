a733760dccb02486d15171083d146baae50ecff4
Isabel Carbonell <carbonei@oregonstate.edu>
2026-04-01 17:22:27 -0700

    complete ascii text convertor

--- asciiArt.js
+++ asciiArt.js
@@ -1,5 +1,13 @@
+const figlet = require('figlet');
+
+
 const {argv} = require('node:process');
 
-
-console.log(argv[2]);
-
+figlet(argv[2], function (err, data){
+	if (err){
+		console.log("Something went wrong");
+		return;
+	}
+	console.log(data);
+	
+});
